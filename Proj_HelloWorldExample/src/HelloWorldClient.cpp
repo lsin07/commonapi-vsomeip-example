2f8dd3cdd6214683e1bed429f374696e02c95e6c
Sein <lsin1227@gmail.com>
2024-04-29 02:43:04 +0000

    fix instance name bug

--- Proj_HelloWorldExample/src/HelloWorldClient.cpp
+++ Proj_HelloWorldExample/src/HelloWorldClient.cpp
@@ -1,4 +1,4 @@
-//HelloWorldClient.cpp
+// HelloWorldClient.cpp
 #include <iostream>
 #include <string>
 #include <unistd.h>
@@ -10,22 +10,35 @@ using namespace v1_0::commonapi;
 int main() {
     // Runtime Instance 가져오기
     std::shared_ptr <CommonAPI::Runtime> runtime = CommonAPI::Runtime::get();
+
     // Proxy 생성 - 위치: "local", 이름: "test"
-    std::shared_ptr <HelloWorldProxy<>> myProxy = runtime->buildProxy<HelloWorldProxy>("local", "test");
+    std::string domain = "local";
+    std::string instance = "commonapi.examples.HelloWorld";
+    std::string connection = "client-sample";
+    std::shared_ptr <HelloWorldProxy<>> myProxy = runtime->buildProxy<HelloWorldProxy>(domain, instance, connection);
+
     // Checking availability (Proxy) : 생성을 기다림 
     std::cout << "Checking availability!" << std::endl;
     while (!myProxy->isAvailable()) {
-        usleep(10);
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
     }
     std::cout << "Available ..." << std::endl;
 
+    const std::string name = "World";
     // 메서드 호출 상태 나타내는 Enum type 변수 선언
     CommonAPI::CallStatus callStatus;
     std::string returnMessage;
-    // CommonAPI의 Proxy 인스턴스인 myProxy로 sayHello 메서드 호출
-    myProxy->sayHello("Cho", callStatus, returnMessage);
-    // 반환 메시지 출력
-    std::cout << "Got message: '" << returnMessage << "'\n";
 
+    while (true) {
+        // CommonAPI의 Proxy 인스턴스인 myProxy로 sayHello 메서드 호출
+        myProxy->sayHello("Cho", callStatus, returnMessage);
+        if (callStatus != CommonAPI::CallStatus::SUCCESS) {
+            std::cerr << "Remote call failed!\n";
+            return -1;
+        }
+        // 반환 메시지 출력
+        std::cout << "Got message: '" << returnMessage << "'\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     return 0;
 }
--- Proj_HelloWorldExample/src/HelloWorldService.cpp
+++ Proj_HelloWorldExample/src/HelloWorldService.cpp
@@ -9,11 +9,16 @@ using namespace std;
 int main() {
     // get() 통해 runtime 인스턴스 획득
     std::shared_ptr <CommonAPI::Runtime> runtime = CommonAPI::Runtime::get();
+
+    std::string domain = "local";
+    std::string instance = "commonapi.HelloWorld";
+    std::string connection = "service-sample";
+
     // HelloWorld 서비스의 Stub 클래스 인스턴스 생성
     std::shared_ptr <HelloWorldStubImpl> myService = std::make_shared<HelloWorldStubImpl>();
     // registerService() 통해 runtime에 서비스 등록
     // 위치: local, 이름: test, 처리: myServie(Stub)
-    runtime->registerService("local", "test", myService);
+    runtime->registerService(domain, instance, myService, connection);
     std::cout << "Successfully Registered Service!" << std::endl;
     
     // 서비스가 클라이언트 호출을 기다림
