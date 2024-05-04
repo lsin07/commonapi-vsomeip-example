e92bc7d0d99bcfdaaf61108ea074d0a585c7cde9
Sein <lsin1227@gmail.com>
2024-05-04 00:27:07 +0000

    add configs for speedcontrol example

--- Proj_SpeedControl/src/SpeedControlStubImpl.cpp
+++ Proj_SpeedControl/src/SpeedControlStubImpl.cpp
@@ -7,14 +7,14 @@ SpeedControlStubImpl::~SpeedControlStubImpl() { }
 using namespace v1::Ace::Someip;
 
 void SpeedControlStubImpl::Warning() {
-    SpeedControl::WarningInfo warning = SpeedControl::WarningInfo::OVERSPEEDWARNING;
+    SpeedControl::WarningInfo warning = SpeedControl::WarningInfo::LOWFUELWARNING;
     while (true) {
         fireWarningEvent(warning);
-        std::cout << "warning broadcasted: OVERSPEEDWARNING" << std::endl;
+        std::cout << "warning broadcasted: LOWFUELWARNING" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(2)); 
     }
 };
 
-void SpeedControlStubImpl::Brake(const std::shared_ptr<CommonAPI::ClientId> _client, uint32_t _breakValue) {
-    std::cout << "Brake requested with value: " << _breakValue << std::endl;
+void SpeedControlStubImpl::Brake(const std::shared_ptr<CommonAPI::ClientId> _client, uint32_t _brakeValue) {
+    std::cout << "Brake requested with value: " << _brakeValue << std::endl;
 };
