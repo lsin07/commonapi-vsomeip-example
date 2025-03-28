fd29c5fcac3c21e475ff30f0d5c0666e173541ca
Sein Lee <lsin1227@gmail.com>
2025-03-28 19:37:33 +0900

    update `build_lib.sh`

--- build_lib.sh
+++ build_lib.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 display_error() {
-    echo "Something went wrong, try again."
+    echo "Build process stopped."
 }
 trap 'display_error; exit 1' ERR
 set -e
 
 PARALLEL_JOBS=${1:-1}
+BUILD_DIR=build
+LIB_DIR=../../lib
+CAPI_CORE_PATH=capicxx-core-runtime
+CAPI_SOMEIP_PATH=capicxx-someip-runtime
+VSOMEIP_PATH=vsomeip
+CMAKE_OPTIONS="-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$LIB_DIR"
+CMAKE_BUILD_OPTIONS="--parallel $PARALLEL_JOBS"
 
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
-  echo "error: parallel_jobs must be numbers"
-  exit 1
+if ! [[ "$PARALLEL_JOBS" =~ ^[0-9]+$ ]]; then
+    echo "error: parallel_jobs must be numbers"
+    exit 1
 fi
 
 # prerequisites
@@ -18,19 +25,13 @@ fi
 # sudo apt upgrade -y
 # sudo apt install -y cmake cmake-qt-gui libexpat-dev expat default-jre libdbus-1-dev libboost-all-dev
 
-cmake -S capicxx-core-runtime/ -B build/capicxx-core-runtime/
-cmake --build build/capicxx-core-runtime/ --parallel "$PARALLEL_JOBS"
+cmake $CMAKE_OPTIONS -S $CAPI_CORE_PATH -B $BUILD_DIR/$CAPI_CORE_PATH
+cmake $CMAKE_OPTIONS -S $VSOMEIP_PATH -B $BUILD_DIR/$VSOMEIP_PATH
+cmake -DUSE_INSTALLED_COMMONAPI=OFF $CMAKE_OPTIONS -S $CAPI_SOMEIP_PATH -B $BUILD_DIR/$CAPI_SOMEIP_PATH
 
-cmake -S vsomeip/ -B build/vsomeip
-cmake --build build/vsomeip --parallel "$PARALLEL_JOBS"
+cmake --build $BUILD_DIR/$CAPI_CORE_PATH $CMAKE_BUILD_OPTIONS
+cmake --build $BUILD_DIR/$VSOMEIP_PATH $CMAKE_BUILD_OPTIONS
+cmake --build $BUILD_DIR/$CAPI_SOMEIP_PATH $CMAKE_BUILD_OPTIONS
 
-cmake -DUSE_INSTALLED_COMMONAPI=OFF -S capicxx-someip-runtime/ -B build/capicxx-someip-runtime/
-cmake --build build/capicxx-someip-runtime/ --parallel "$PARALLEL_JOBS"
-
-# copy libraries into common library directory
-mkdir -p lib
-cp build/capicxx-core-runtime/lib* lib/
-cp build/vsomeip/lib* lib/
-cp build/capicxx-someip-runtime/lib* lib/
-
-echo "Build Process Finished Successfully."
+# clean
+rm -rf build/
