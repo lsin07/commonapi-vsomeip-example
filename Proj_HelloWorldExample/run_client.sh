e261a0a03c42160ff9edc08202957d56aebd715a
Sein Lee <lsin1227@gmail.com>
2025-05-11 20:52:14 +0900

    add easy switch to external networking for automated scripts

--- Proj_HelloWorldExample/run_client.sh
+++ Proj_HelloWorldExample/run_client.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-CONFIG=configs/config_client.json
-# get current IP adddress
-CURRENT_IP=$(hostname -I | awk '{print $1}')
+COMMONAPI_CONFIG=commonapi4someip.ini
+LD_LIBRARY_PATH=../install_folder/lib:$PWD/build/
 
-if [ -z "$CURRENT_IP" ]; then
-    echo "Unable to get IP address."
-    exit 1
-fi
+if [ "$1" = "--external" ]; then
+    CONFIG=configs/config_client.json
+    CURRENT_IP=$(hostname -I | awk '{print $1}')
 
-if command -v jq &> /dev/null; then
-    jq ".unicast = \"$CURRENT_IP\"" $CONFIG > tmp.json && mv tmp.json $CONFIG
+    if [ -z "$CURRENT_IP" ]; then
+        echo "Unable to get IP address."
+        exit 1
+    fi
+
+    if command -v jq &> /dev/null; then
+        jq ".unicast = \"$CURRENT_IP\"" $CONFIG > tmp.json && mv tmp.json $CONFIG
+    else
+        # if jq is not installed.
+        sed -i "s/\"unicast\" : \"[^\"]*\"/\"unicast\" : \"$CURRENT_IP\"/" $CONFIG
+    fi
+
+    COMMONAPI_CONFIG=commonapi4someip.ini LD_LIBRARY_PATH=../install_folder/lib:$PWD/build/ VSOMEIP_CONFIGURATION=$CONFIG build/HelloWorldClient
 else
-    # if jq is not installed.
-    sed -i "s/\"unicast\" : \"[^\"]*\"/\"unicast\" : \"$CURRENT_IP\"/" $CONFIG
+    COMMONAPI_CONFIG=commonapi4someip.ini LD_LIBRARY_PATH=../install_folder/lib:$PWD/build/ build/HelloWorldClient
 fi
-
-COMMONAPI_CONFIG=commonapi4someip.ini LD_LIBRARY_PATH=../install_folder/lib:$PWD/build/ VSOMEIP_CONFIGURATION=$CONFIG build/HelloWorldClient
